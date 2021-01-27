af8ba0da8db8e1b6ad4cdcf84dceb921ea30b21f
Marcio Kiyohideki Komesu <marciokiyo@gmail.com>
2021-01-26 22:01:24 -0300

    Aula 1

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,34 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
+import db from '../db.json'
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
-    box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${db.theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
   }
 `
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={db.theme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizLogo></QuizLogo>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            Header
+          </Widget.Header>
+          <Widget.Content>
+            Content
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            Header
+          </Widget.Header>
+          <Widget.Content>
+            Content
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/marciokiyo/mquiz"></GitHubCorner>
+    </QuizBackground>
+  )
 }
